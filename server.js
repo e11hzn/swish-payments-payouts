8040173363d2281f22c8d2516348d0af52f61593
e11hzn <haitham.zaben@elva11.se>
2025-12-15 11:04:13 +0100

    feat: initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const { createServer } = require('https');
+const { parse } = require('url');
+const next = require('next');
+const fs = require('fs');
+require('dotenv').config({ path: '.env.local' });
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const httpsOptions = {
+  key: fs.readFileSync(process.env.SSL_KEY_PATH),
+  cert: fs.readFileSync(process.env.SSL_CERT_PATH),
+};
+
+app.prepare().then(() => {
+  createServer(httpsOptions, (req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(3000, (err) => {
+    if (err) throw err;
+    console.log('> Ready on https://localhost:3000');
+  });
+});
---
+++ setup.md
@@ -0,0 +1,6 @@
+# Setup
+
+- In this folder, create a NextJS project with Typescript and React using yarn according to https://nextjs.org/docs/app/getting-started/installation
+- The dev-server should run with HTTPS to allow some integration with external APIs
+- The SSL certificate should be created and the values needed for the SSL dev-server should be added as environment variables
+  - For local development, add these environment variables to the .env.local file
